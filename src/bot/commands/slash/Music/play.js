9ed9affc3a8fdf251e99a31d0ddce13642eb10e0
Cetrei#0636 <113970865+Cetrei@users.noreply.github.com>
2024-11-21 12:41:16 -0600

    Complete Merge

--- src/bot/commands/slash/Music/play.js
+++ src/bot/commands/slash/Music/play.js
@@ -592,7 +592,7 @@ async function playSong(queue, Interaction, playlist = false) {
     }
 
     const song = queue.songs.shift();
-    console.log(`Intentando reproducir: ${song}`);
+    console.log(`Intentando reproducir: ${song.url}`);
     let resource;
 
     try {
